b5d57de9e412602ba927458dcb5687d1ecb8936e
Caleb Zulawski <caleb.zulawski@gmail.com>
2015-05-22 01:45:48 -0400

    Update makefile to include threading

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	srand(time(NULL));
-	bool color = rand() % 2;
+	//bool color = rand() % 2;
 	
-	auto game = new GameWrapper(true, true);
+	auto game = new GameWrapper(true, false);
 	game->run();
 }
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC=g++
 CFLAGS=-std=c++11 -c -Wall
-LDFLAGS=
+LDFLAGS=-pthread
 SOURCES=wrappers.cpp ai.cpp board.cpp main.cpp
 OBJECTS=$(SOURCES:.cpp=.o)
 EXECUTABLE=checkers-ai
